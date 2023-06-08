db1e9d64f7b5b9f4d01515a8cf8247d8bc529eca
Temaker11 <temaker11.dev@gmail.com>
2023-06-08 15:13:49 -0300

    Play time

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function carregar() {
   let msg = window.document.getElementById("msg");
   let img = window.document.getElementById("imagem");
   let data = new Date();
-  /* let hora = data.getHours(); */
-  let hora = 20
+  let hora = data.getHours();
+
   msg.innerHTML = `Agora são ${hora} horas.`;
   if (hora >= 0 && hora < 12) {
     // Bom dia
