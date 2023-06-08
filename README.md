522d946e7b0198ea9fdbf9f44939a5db64724892
Temaker11 <temaker11.dev@gmail.com>
2023-06-08 15:04:28 -0300

    Simple Time

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center"> Hora do Dia!
+
+<p align="center">
+  <img alt="imagem" src="./Mostrar.png"> 
---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar() {
+  let msg = window.document.getElementById("msg");
+  let img = window.document.getElementById("imagem");
+  let data = new Date();
+  /* let hora = data.getHours(); */
+  let hora = 20
+  msg.innerHTML = `Agora são ${hora} horas.`;
+  if (hora >= 0 && hora < 12) {
+    // Bom dia
+    img.src = "./img/dia .png";
+    document.body.style.background = "#4563f7";
+  } else if (hora >= 12 && hora < 18) {
+    // Boa tarde
+    img.src = "./img/tarde.png";
+    document.body.style.background = "#f1f502";
+  } else {
+    // Boa noite
+    img.src = "./img/noite .png";
+    document.body.style.background = "#111442";
+  }
+}
