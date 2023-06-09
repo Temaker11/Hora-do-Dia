24e19a45dbe6ab237cdeda08969a16da07771cc9
Temaker11 <temaker11.dev@gmail.com>
2023-06-09 13:39:05 -0300

    Resolvendo Horario

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function carregar() {
   let hora = data.getHours();
 
   msg.innerHTML = `Agora são ${hora} horas.`;
-  if (hora >= 0 && hora < 12) {
+  if (hora >= 5 && hora < 12) {
     // Bom dia
     img.src = "./img/dia .png";
     document.body.style.background = "#4563f7";
-  } else if (hora >= 12 && hora < 18) {
+  } else if (hora >= 12 && hora <= 18) {
     // Boa tarde
     img.src = "./img/tarde.png";
     document.body.style.background = "#f1f502";
-  } else {
+  } else if ((hora >= 19 && hora <= 23) || (hora >= 0 && hora <= 4)) {
     // Boa noite
     img.src = "./img/noite .png";
     document.body.style.background = "#111442";
